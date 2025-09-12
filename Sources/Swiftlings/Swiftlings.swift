76f9a9dd2c6cecfc77272dd95600db9b55fcb310
Tornike Gomareli <24585160+tornikegomareli@users.noreply.github.com>
2025-09-13 01:16:56 +0400

    chore: bump version to v0.1.2

--- Sources/Swiftlings/Swiftlings.swift
+++ Sources/Swiftlings/Swiftlings.swift
@@ -4,7 +4,7 @@ public struct Swiftlings: ParsableCommand {
   public static let configuration = CommandConfiguration(
     commandName: "swiftlings",
     abstract: "A Swift learning tool inspired by Rustlings",
-    version: "0.1.1",
+    version: "0.1.2",
     subcommands: [
       InitCommand.self,
       RunCommand.self,
